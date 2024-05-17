b7286be3fae41d220b7b81736396bb9abace3e20
Spicfy <68024083+Spicfy@users.noreply.github.com>
2024-05-17 17:07:14 -0400

    finished hero component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import {Navbar} from './layouts/NavbarAndFooter/Navbar';
 import {ExploreTopBooks} from './layouts/HomePage/ExploreTopBooks'
 import {Carousel} from './layouts/HomePage/Carousel'
+import { Heros } from './layouts/HomePage/Heros';
 function App() {
   return (
     <div>
       <Navbar/>
       <ExploreTopBooks/>
       <Carousel/>
+      <Heros/>
     </div>
   );
 }
